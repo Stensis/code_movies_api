ed67f2e016972e6c48a10faec92ab8c8aded59f6
Stensis <irenenjuguna98@gmail.com>
2023-03-27 03:18:13 +0300

    added seed file for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# users
+User.create(name: "John Smith", contact: 12345678, email: "john.smith@example.com", password_digest: "password",confirmation_password_digest:"password")
+User.create(name: "Jane Doe", contact: 29876543, email: "jane.doe@example.com", password_digest: "password",confirmation_password_digest:"password")
+User.create(name: "Bob Johnson", contact: 5551234, email: "bob.johnson@example.com", password_digest: "password",confirmation_password_digest:"password")
+User.create(name: "Alice Brown", contact: 55598765, email: "alice.brown@example.com", password_digest: "password",confirmation_password_digest:"password")
+User.create(name: "Sam Williams", contact: 55555512, email: "sam.williams@example.com", password_digest: "password",confirmation_password_digest:"password")
